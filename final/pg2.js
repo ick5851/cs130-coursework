6af46650a208bc846aca871cab7dba547e6a4ba5
Isabella Ko <Isabellako@MacBook-Pro-5.hsd1.ga.comcast.net>
2020-06-03 18:28:35 -0400

    my final so far

--- final/pg2.js
+++ final/pg2.js
@@ -1,20 +1,9 @@
-let activePoint = null;
-
-const showInfo = (e) => {
-    activePoint = e.target; //element we clicked on
-    const selectedInfo = activePoint.nextElementSibling.innerHTML;
-    if () {
-      //to get the info to show up and go away when a point is clicked (will need to )
-    } else {
-
-    }
-};
-
-
-const points = document.querySelectorAll('.point');
-for (point of points) {
-  point.onclick = showInfo;
- }
-};
-
-}
+$(function(){
+  $().timelinr({
+    orientation: 	'vertical',
+    issuesSpeed: 	300,
+    datesSpeed: 	100,
+    arrowKeys: 		'true',
+    startAt:		1
+  })
+});
